3dfa21009942f5de0e8ab8363b6b56f2f02f299d
Shruti Narad <121679234+shrutinarad16@users.noreply.github.com>
2025-04-25 22:36:54 +0530

    Routes are added with v6, version has been updated- routes, route and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Navbar from './components/Navbar';
-//import About from './components/About';
-import TextForm from './components/TextForm';
-import React, { useState } from 'react';
-import Alert from './components/Alert';
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import {
+  BrowserRouter as Router,
+  Routes, 
+  Route
+} from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+  const [mode, setMode] = useState("light"); // whether dark mode is enabled or not
 
   const [alert, setAlert] = useState(null);
 
@@ -18,46 +23,55 @@ function App() {
       setAlert(null);
     }, 2000);
   };
-  const toggleMode = ()=>{
-    if(mode === 'dark')
-    {
-    setMode('light')
-    document.body.style.backgroundColor = 'white';
-    showAlert("Light mode has been enabled", "success");
-    document.title = "TextUtils - Light Mode";
-    setInterval(() => {
-      document.title = "TextUtils is Amazing";
-    },2000);
-    // setInterval(() => {
-    //   document.title = "Install TextUtils Now";
-    // },1500);
-    }
-    else
-    {
-      setMode('dark')
-      document.body.style.backgroundColor = '#042743';
+  const toggleMode = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
+      setInterval(() => {
+        document.title = "TextUtils is Amazing";
+      }, 2000);
+      // setInterval(() => {
+      //   document.title = "Install TextUtils Now";
+      // },1500);
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
       document.title = "TextUtils - Dark Mode";
       setInterval(() => {
         document.title = "TextUtils is Amazing";
-      },2000);
+      }, 2000);
       // setInterval(() => {
       //   document.title = "Install TextUtils Now";
       // },1500);
     }
-
-  }
+  };
   return (
-    <> 
-<Navbar title = "TextUtils" mode ={mode} toggleMode={toggleMode}/>
-<Alert alert={alert}/>
-<div className="container my-3">
-<TextForm showAlert = {showAlert}heading = "Enter text to Analyze" mode={mode}/>
-
-</div>
-
+    <>
+    <Router>
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route
+            exact path="/"
+            element={
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter text to Analyze"
+                mode={mode}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
     </>
   );
 }
 
 export default App;
+
